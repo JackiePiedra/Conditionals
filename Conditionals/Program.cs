0cf000463c8834369cab6537f30fd01fac279004
JackiePiedra <jackie.r.piedra@gmail.com>
2017-08-18 02:18:41 -0400

    Mike and Diners Problem - If/Then Discount

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -10,6 +10,7 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
+            /*
             //conditional example
             //int age = 15;
             Console.WriteLine("Please enter your age");
@@ -21,13 +22,29 @@ namespace Conditionals
             else
             {
                 Console.WriteLine("Sorry! You're too young! :(");
-            }
-            
-            /*//Collect info, number of diners and cost per diner
-            Console.WriteLine("Please enter the number of diners:");
-            double numberDiners = double.Parse(Console.ReadLine());
+            }*/
+
+            //user enters the number of diners and cost per diner
+            Console.WriteLine("Please enter the number of diners in your party:");
+            int numberDiners = int.Parse(Console.ReadLine());
             Console.WriteLine("Please enter the cost per diner:");
-            double costPerDiner = double.Parse(Console.ReadLine());*/
+            double costPerDiner = double.Parse(Console.ReadLine());
+
+            //calculating the total bill
+            double totalBill = numberDiners * costPerDiner;
+
+            //conditional to determine which discount they get
+            if(totalBill >=50)
+            {
+                double discountedBill = totalBill * .9;
+                Console.WriteLine("Thank you for dining with us! \nYou have received a 10% discount as part of our Customer Appreciation Week. \nYour total bill is "+ discountedBill+".");
+            }
+            else
+            {
+                double discountedBill = totalBill * .95;
+                Console.WriteLine("Thank you for dining with us! \nYou have received a 5% discount as part of our Customer Appreciation Week. \nYour total bill is "+ discountedBill+ ".");
+            }
+           
         }
     }
 }
