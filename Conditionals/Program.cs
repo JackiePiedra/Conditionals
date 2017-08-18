2b327d026712b286ba395fb26e2814b28a0631fc
JackiePiedra <jackie.r.piedra@gmail.com>
2017-08-17 20:52:14 -0400

    Conditionals

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -10,6 +10,24 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
+            //conditional example
+            //int age = 15;
+            Console.WriteLine("Please enter your age");
+            int age = int.Parse(Console.ReadLine());
+            if(age>=17)
+            {
+                Console.WriteLine("You can see this movie.");
+            }
+            else
+            {
+                Console.WriteLine("Sorry! You're too young! :(");
+            }
+            
+            /*//Collect info, number of diners and cost per diner
+            Console.WriteLine("Please enter the number of diners:");
+            double numberDiners = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the cost per diner:");
+            double costPerDiner = double.Parse(Console.ReadLine());*/
         }
     }
 }
