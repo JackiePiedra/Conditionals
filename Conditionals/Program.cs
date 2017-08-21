cd631b97191c4c156b34eb079725dfe4e72b82b8
JackiePiedra <jackie.r.piedra@gmail.com>
2017-08-21 19:49:27 -0400

    Additional conditionals word problems

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -15,7 +15,15 @@ namespace Conditionals
             //int age = 15;
             Console.WriteLine("Please enter your age");
             int age = int.Parse(Console.ReadLine());
-            if(age>=17)
+            if (age == 21)
+            {
+                Console.WriteLine("Let's get a drink");
+            }
+            else if (age ==18)
+            {
+                Console.WriteLine("You are now an adult");
+            }
+            else if (age>=17)
             {
                 Console.WriteLine("You can see this movie.");
             }
@@ -24,27 +32,88 @@ namespace Conditionals
                 Console.WriteLine("Sorry! You're too young! :(");
             }*/
 
-            //user enters the number of diners and cost per diner
+            
+            //ask the user to enter the number of diners and cost per diner
             Console.WriteLine("Please enter the number of diners in your party:");
             int numberDiners = int.Parse(Console.ReadLine());
             Console.WriteLine("Please enter the cost per diner:");
             double costPerDiner = double.Parse(Console.ReadLine());
-
+            
             //calculating the total bill
             double totalBill = numberDiners * costPerDiner;
-
+            /* 
             //conditional to determine which discount they get
             if(totalBill >=50)
             {
                 double discountedBill = totalBill * .9;
-                Console.WriteLine("Thank you for dining with us! \nYou have received a 10% discount as part of our Customer Appreciation Week. \nYour total bill is "+ discountedBill+".");
+                Console.WriteLine("Thank you for dining with us! \nYou have received a 10% discount as part of our Customer Appreciation Week. \nYour total bill is $"+ discountedBill+". The total per guest is $" +(discountedBill/numberDiners)+".");
             }
             else
             {
                 double discountedBill = totalBill * .95;
-                Console.WriteLine("Thank you for dining with us! \nYou have received a 5% discount as part of our Customer Appreciation Week. \nYour total bill is "+ discountedBill+ ".");
+                Console.WriteLine("Thank you for dining with us! \nYou have received a 5% discount as part of our Customer Appreciation Week. \nYour total bill is $"+ discountedBill+". The total per guest is $" + (discountedBill / numberDiners) + ".");
+            } */
+
+            double discount = 0.0;
+            if(totalBill >= 50)
+            {
+                discount = 1.10;
+                totalBill /= discount;
             }
+            else
+            {
+                discount = 1.05;
+                totalBill = totalBill / discount;
+            }
+
+            double perGuest = totalBill / numberDiners;
+            Console.WriteLine("Thank you for dining with us! Your new total is $"+totalBill+ ". Each guest will pay $"+perGuest+".");
            
+            /*Console.WriteLine("Enter your percentage");
+            int grade = int.Parse(Console.ReadLine());
+            if (grade >= 90 && grade <= 100)
+            {
+                Console.WriteLine("You got an A!");
+            }
+            else if (grade >= 80 && grade <= 89)
+            {
+                Console.WriteLine("You got a B! Not Bad");
+            }*/
+
+            Console.WriteLine("Please enter your age:");
+            int age = int.Parse(Console.ReadLine());
+            if (age <= 2)
+            {
+                Console.WriteLine("Still in Mama's arms");
+            }
+            else if (age >=3 && age <= 4)
+            {
+                Console.WriteLine("Preschool Maniac");
+            }
+            else if(age >=5 && age <= 11)
+            {
+                Console.WriteLine("Elementary School");
+            }
+            else if(age >=12 && age <=14)
+            {
+                Console.WriteLine("Middle School");
+            }
+            else if (age >=15 && age <= 18)
+            {
+                Console.WriteLine("High School");
+            }
+            else if (age >19 && age <=22)
+            {
+                Console.WriteLine("College");
+            }
+            else if(age >=23 && age <=65)
+            {
+                Console.WriteLine("Working for the Man");
+            }
+            else if (age >=66)
+            {
+                Console.WriteLine("The Golden Years");
+            }
         }
     }
 }
